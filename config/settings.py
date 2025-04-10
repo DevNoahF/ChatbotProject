e4212ff709b0df5edc2dde7b53b2b0f1fb4c823b
Noah Vêri Franco <noahvf16@outlook.com>
2025-04-10 19:25:05 -0300

    Update: Rename apy key and api code

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,4 @@
-# config/settings.py
+# config/Settings.py
 # gpt-4o-mini
 import os
 from dotenv import load_dotenv
@@ -6,5 +6,5 @@ from dotenv import load_dotenv
 load_dotenv()  # Carregar variáveis do .env
 
 # gpt - 3.5 turbo
-OPENIA_API_KEY = os.getenv("OPENIAROUTER_API_KEY")
+ROUTEROPENIA_API_KEY = "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"
 MAX_TOKENS = 20
