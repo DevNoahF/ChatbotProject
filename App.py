ed15e6b79aec32914fd81a7248334d7d7995579c
Leonardo Perin <155500125+Perinzinho@users.noreply.github.com>
2025-04-10 20:14:11 -0300

    Added parameters for the bot need to put the function chat on settings.py

--- App.py
+++ App.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
 
-from config.Settings import ROUTEROPENIA_API_KEY
+from config.settings import ROUTEROPENIA_API_KEY
 
 app = Flask(__name__)
 CORS(app)  # Permite que o frontend acesse a API
@@ -17,7 +17,16 @@ def chat():
     response = requests.post(
         "https://openrouter.ai/api/v1/chat/completions",
         headers={"Authorization": f"Bearer {ROUTEROPENIA_API_KEY}", "Content-Type": "application/json"},
-        json={"model": "openai/gpt-3.5-turbo", "messages": [{"role": "user", "content": user_message}]}
+        json={"model": "openai/gpt-3.5-turbo", "messages": [
+            {"role": "user", "content": user_message},
+            {"role": "user", "content": "Você é um bot de atendimento ao cliente, seja educado, claro e objetivo"},
+            #Contexto para ele, mais tarde a gente adiciona as coisas
+        ],
+            "temperature":0.3,#Define
+            "max_tokens":50, #Define quantidade de caracteres
+            "presence_penalty":1.0 #Define penalidade por sair do assunto
+
+        }
     )
 
     bot_reply = response.json()["choices"][0]["message"]["content"]
