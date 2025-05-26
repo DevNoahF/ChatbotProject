423a8163c894ca5e4d9f52c2ee6f84b35b5f1032
GustavosJesusSec <31715001+GustavosJesusSec@users.noreply.github.com>
2025-05-26 20:30:12 -0300

    install do nltk

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()  # Carregar variáveis do .env
 
 # gpt - 3.5 turbo
-ROUTEROPENIA_API_KEY = "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"
+ROUTEROPENIA_API_KEY = "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"
 
 def change_tokens(mensagem):#Função que altera os tokens, leva em conta a quantidade de palavras presente na mensagem do usuário
     mensagem.split()
---
+++ install.py
@@ -0,0 +1,4 @@
+# Salve num arquivo Python separado ou rode no terminal interativo
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
